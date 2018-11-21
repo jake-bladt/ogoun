232f046dc4d0ea158140c313a03badf04fb4bc96
Jake Bladt <jake@jakebladt.com>
2018-11-20 22:04:32 -0500

    pass-through url and depth

--- lambdas/enqueue-request.py
+++ lambdas/enqueue-request.py
@@ -1,12 +1,22 @@
-import os
 import boto3
+import json
+import os
 
 SQS_CLIENT = boto3.client('sqs')
 queue_url = os.getenv('SQS_URL')
 
 def lambda_handler(event, context):
+    url = event['url']
+    depth = event['depth']
+    
+    qmessage = {}
+    qmessage["url"] = url
+    qmessage["depth"] = depth
+    
+    print(json.dumps(qmessage))
+    
     print(SQS_CLIENT.send_message(
         QueueUrl=queue_url,
-        MessageBody='sent message'
+        MessageBody=json.dumps(qmessage)
     ))
     return ''
