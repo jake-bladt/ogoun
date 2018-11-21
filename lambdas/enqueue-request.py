037e0e65aca9f2bed77d5968580944ad3eefe6ec
Jake Bladt <jake@jakebladt.com>
2018-11-20 21:46:04 -0500

    -_

--- lambdas/enqueue-request.py
+++ lambdas/enqueue-request.py
@@ -1,9 +1,12 @@
 import os
 import boto3
 
+SQS_CLIENT = boto3.client('sqs')
+queue_url = os.getenv('SQS_URL')
+
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    print(SQS_CLIENT.send_message(
+        QueueUrl=queue_url,
+        MessageBody='sent message'
+    ))
+    return ''
